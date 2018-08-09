34de07483493f13587c8dcbb305380bdb517a909
dkreidler <dkreidler@gmail.com>
2018-08-08 20:56:14 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,114 @@
+// setup canvas
+
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min, max) {
+    var num = Math.floor(Math.random() * (max - min)) + min;
+    return num;
+}
+
+function Shape(x, y, velX, velY, exists) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.exists = exists;
+}
+// define Ball constructor, inheriting from Shape
+function Ball(x, y, velX, velY, exists, color, size) {
+    Shape.call(this, x, y, velX, velY, exists);
+
+    this.color = color;
+    this.size = size;
+}
+
+Ball.prototype = Object.create(Shape.prototype);
+Ball.prototype.constructor = Ball;
+
+Ball.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+};
+/*
+In the four cases, we are:
+
+Checking to see whether the x coordinate is greater than the width of the canvas
+     (the ball is going off the right hand edge).
+Checking to see whether the x coordinate is smaller than 0 
+    (the ball is going off the left hand edge).
+Checking to see whether the y coordinate is greater than the height of the canvas 
+    (the ball is going off the bottom edge).
+Checking to see whether the y coordinate is smaller than 0 
+    (the ball is going off the top edge).
+ */
+Ball.prototype.update = function() {
+    if ((this.x + this.size) >= width) {
+        this.velX = -(this.velX);
+    }
+    if ((this.x - this.size) <= 0) {
+        this.velX = -(this.velX);
+    }
+    if ((this.y + this.size) >= height) {
+        this.velY = -(this.velY);
+    }
+    if ((this.y - this.size) <= 0) {
+        this.velY = -(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+
+};
+
+Ball.prototype.collisionDetect = function() {
+    for (var j = 0; j < balls.length; j++) {
+        if (!(this === balls[j])) {
+            var dx = this.x - balls[j].x;
+            var dy = this.y - balls[j].y;
+            var distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + balls[j].size) {
+                balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+            }
+        }
+    }
+};
+
+var balls = [];
+
+function loop() {
+    ctx.fillStyle = "rgba(0, 0, 124, 0.2)";
+    ctx.fillRect(0, 0, width, height);
+
+    while (balls.length < 25) {
+        var size = random(10, 25);
+        var ball = new Ball(
+            // ball position always drawn at least one ball width
+            // away from the edge of the canvas, to avoid drawing errors
+            random(0 + size, width - size),
+            random(0 + size, height - size),
+            random(-7, 7),
+            random(-7, 7),
+            true,
+            'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
+            size
+        );
+        balls.push(ball);
+    }
+    for (var i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].update();
+        balls[i].collisionDetect();
+    }
+
+    requestAnimationFrame(loop);
+}
+loop();
